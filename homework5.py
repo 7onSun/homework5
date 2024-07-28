31ac6ab137d472e8572486e95967bd544e8b67c4
7onSun <7onsun@mail.ru>
2024-07-28 22:52:23 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,6 @@
+immutable_var = 'World', 2, True, 'Оле-оле-оле', 55, 7
+print('Immutable tuple: ', immutable_var)
+mutable_list = ['Паралон', 'Бульон', "Ореховый борщ", 4, 88, False]
+mutable_list[0] = 'Забияка'
+mutable_list .extend([55, 'snowball', 24.6])
+print('Mutable list: ',mutable_list)
